73fa55808800f070c21ab4855572173341c70f74
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2022-11-24 06:11:23 +0000

    prettier

--- iLike.js
+++ iLike.js
@@ -2,9 +2,14 @@ const { readFile } = require("node:fs/promises")
 const { Listr } = require("listr2")
 require("dotenv").config()
 
-const getPlaylist = async (plid, token="") => {
+const getPlaylist = async (plid, token = "") => {
     try {
-        let res = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${plid}&key=${process.env.API_KEY}&maxResults=50${token && "&pageToken=" + token}`, {"Accept": "application/json"})
+        let res = await fetch(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${plid}&key=${
+                process.env.API_KEY
+            }&maxResults=50${token && "&pageToken=" + token}`,
+            { Accept: "application/json" }
+        )
         if (!res.ok) console.error("PLAYLIST CONTENTS NOT OKAY\n", res)
         return await res.json()
     } catch (e) {
@@ -14,7 +19,10 @@ const getPlaylist = async (plid, token="") => {
 
 const getPlaylistDetails = async (plid) => {
     try {
-        let res = await fetch(`https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${plid}&key=${process.env.API_KEY}`, {"Accept": "application/json"})
+        let res = await fetch(
+            `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${plid}&key=${process.env.API_KEY}`,
+            { Accept: "application/json" }
+        )
         if (!res.ok) console.error("PLAYLIST DETAILS NOT OKAY!\n", res)
         return await res.json()
     } catch (e) {
@@ -28,42 +36,50 @@ const main = async () => {
 
     try {
         ids = JSON.parse(await readFile("playlists.json"))
-        await new Listr(ids.map((plid,i) => {return {
-            title: `playlist ${plid}`,
-            task: (_,task)=>task.newListr(
-                [{
-                    title: "get playlist details",
-                    task: async () => {
-                        let resp = await getPlaylistDetails(plid)
-                        task.title = resp.items[0].snippet.title.length > 60 ? resp.items[0].snippet.title.slice(0, 60) + "…" : resp.items[0].snippet.title.padEnd(61, " ")
-                    }
-                },
-                {
-                    title: "get all videos in playlist",
-                    task: async () => {
-                        let resp
-                        let vids = []
-                        let token = ""
-                        do {
-                            resp = await getPlaylist(plid, token)
-                            vids = vids.concat(resp.items)
-                            token = resp.nextPageToken
-                            task.title = `${task.title.slice(0,61)} found ${vids.length}`
-                        } while (token)
-                        playlists[i] = vids
-                        task.title = `${task.title.slice(0,61)} found ${vids.length}`
-                    }
-                }],
-                { rendererOptions: { collapse: false }}
-            )
-            }}),
+        await new Listr(
+            ids.map((plid, i) => {
+                return {
+                    title: `playlist ${plid}`,
+                    task: (_, task) =>
+                        task.newListr(
+                            [
+                                {
+                                    title: "get playlist details",
+                                    task: async () => {
+                                        let resp = await getPlaylistDetails(plid)
+                                        task.title =
+                                            resp.items[0].snippet.title.length > 60
+                                                ? resp.items[0].snippet.title.slice(0, 60) + "…"
+                                                : resp.items[0].snippet.title.padEnd(61, " ")
+                                    },
+                                },
+                                {
+                                    title: "get all videos in playlist",
+                                    task: async () => {
+                                        let resp
+                                        let vids = []
+                                        let token = ""
+                                        do {
+                                            resp = await getPlaylist(plid, token)
+                                            vids = vids.concat(resp.items)
+                                            token = resp.nextPageToken
+                                            task.title = `${task.title.slice(0, 61)} found ${vids.length}`
+                                        } while (token)
+                                        playlists[i] = vids
+                                        task.title = `${task.title.slice(0, 61)} found ${vids.length}`
+                                    },
+                                },
+                            ],
+                            { rendererOptions: { collapse: false } }
+                        ),
+                }
+            }),
             { concurrent: true }
         ).run()
     } catch (e) {
         console.error(e)
     }
     // console.log(playlists, playlists.length)
-
 
     // now do the likes
 }
